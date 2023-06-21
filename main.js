80ad3cb70f8ca8bb74cd86235b53f80102909026
CandidaM <64532668+CandidaM@users.noreply.github.com>
2023-06-21 09:43:39 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function startClassification()
+{
+  
+}
+
+function modelReady(){
+  
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    
+
+    if (results[0].label == "Palmas") {
+      
+    } else if (results[0].label == "Sino") {
+      
+    } else if (results[0].label == "Estalos") {
+     
+    }else{
+     
+    }
+  }
+}
